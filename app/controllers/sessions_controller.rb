83b7c76012ac972f75839909186de3a59beedd63
Ian Lollar <ian.lollar@gmail.com>
2012-11-10 12:37:03 -0800

    Add main sidebar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to login_url
+      redirect_to root_path
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_url, notice: 'Logged out'
+    redirect_to root_path, notice: 'Logged out'
   end
 end
