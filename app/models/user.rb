b2174fceab169056df472e297c9c81809ff96d4d
Ian Lollar <ian.lollar@gmail.com>
2012-11-24 17:43:25 -0800

    Fix user validation bug to fix voting issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true, :format => { :with => /.+@.+\..+/}
   validates :name, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true
-  validates :password_confirmation, presence: true
 
   has_secure_password
 end
