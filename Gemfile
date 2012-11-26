ca8f1a2dacb63f068e72f473f05d96e46bec56ba
Ian Lollar <ian.lollar@gmail.com>
2012-11-25 21:51:17 -0800

    Add New Relic

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jquery-rails'
 
 gem 'thin'
 
+gem 'newrelic_rpm'
+
 # To use ActiveModel has_secure_password
 gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/application.rb
+++ config/application.rb
@@ -66,6 +66,8 @@ module CollaborativeCursing
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     if Rails.configuration.respond_to?(:sass)
         Rails.configuration.sass.tap do |config|
             config.preferred_syntax = :sass
